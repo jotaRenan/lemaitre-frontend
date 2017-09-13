867f019b79a52d9d55eadced23b33601474b19d3
Haddadson <gabriel.haddad15@gmail.com>
2017-09-12 21:54:19 -0300

    Adds router to pages

--- app/app.js
+++ app/app.js
@@ -1,8 +1,56 @@
 angular.module("leMaitre", ["ui.router"]).config(function($locationProvider, $urlRouterProvider, $stateProvider){
-    $stateProvider.state('home', {
-      url: '/',
-      templateUrl: 'app/components/shared/login.html',
-    });
+    $stateProvider
+    	// Rota para a página Login
+	    .state('home', {
+	      url: '/',
+	      templateUrl: 'app/components/shared/login.html',
+	    })
+	    //Rota para a página Acompanhar Pedido
+	    .state('acompanhar-pedido', {
+	      url: '/acompanhar-pedido',
+	      templateUrl: 'app/components/client/acompanhar-pedido/acompanhar-pedido.html',
+	    })
+	    //Rota para a página Finalizar Pedido
+	    .state('finalizar-pedido', {
+	      url: '/finalizar-pedido',
+	      templateUrl: 'app/components/client/finalizar-pedido/finalizar-pedido.html',
+	    })
+	    //Rota para a página Cozinha
+	    .state('kitchen', {
+	      url: '/kitchen',
+	      templateUrl: 'app/components/kitchen/index.html',
+	    })
+	    //Rota para a página Edição de Reserva
+	    .state('edicao-reserva', {
+	      url: '/edicao-reserva',
+	      templateUrl: 'app/components/manager/edicao-reserva/edicao-reserva.html',
+	    })
+	    //Rota para a página Gerador de Tokens
+	    .state('gerador-token', {
+	      url: '/gerador-token',
+	      templateUrl: 'app/components/manager/gerador-token/gerador-token.html',
+	    })
+	    //Rota para a página Gestão de Mesas
+	    .state('gestao-mesas', {
+	      url: '/gestao-mesas',
+	      templateUrl: 'app/components/manager/gestao-mesas/gestao-mesas.html',
+	    })
+	    //Rota para a página Status: Mesa Livre
+	    .state('status-mesa-livre', {
+	      url: '/status-mesa-livre',
+	      templateUrl: 'app/components/manager/status-mesa-livre/status-mesa-livre.html',
+	    })
+	    //Rota para a página Status: Mesa Ocupada
+	    .state('status-mesa-ocupada', {
+	      url: '/status-mesa-ocupada',
+	      templateUrl: 'app/components/manager/status-mesa-ocupada/status-mesa-ocupada.html',
+	    })
+	    //Rota para a página Status: Mesa Reservada
+	    .state('status-mesa-reservada', {
+	      url: '/status-mesa-reservada',
+	      templateUrl: 'app/components/manager/status-mesa-reservada/status-mesa-reservada.html',
+	    });
+
     $urlRouterProvider.otherwise('/');
     $locationProvider.html5Mode(true);
 });
