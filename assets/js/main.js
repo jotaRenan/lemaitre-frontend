a03055bddb253eea18cd4ff5d6a1806a26326eb7
Joao Pedro Renan <jota.renan@gmail.com>
2017-09-14 12:44:13 -0300

    responsive menu now hides after item is clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,15 @@
-/*
+const addToggleMenuEvent = (DOMObject) => {
+  DOMObject.addEventListener('click', function() {
+    document.querySelector("body").classList.toggle("with--sidebar");
+  });
+};
+
 window.onload = function() {
-    document.querySelector("#header__icon").click(function(e) {
-      e.preventDefault();
-      document.querySelector("body").classList.toggle("with--sidebar");
-    });
-    document.querySelector("#site-cache").addEventListener('click', function(e) {
+    addToggleMenuEvent(document.querySelector("#header__icon"));
+
+    document.querySelector("#site-cache").addEventListener('click', function() {
       document.querySelector("body").classList.remove("with--sidebar");
     });
-};
-*/
-$(document).ready(function() {
-  (function($) {
-    $("#header__icon").click(function(e) {
-      e.preventDefault();
-      $("body").toggleClass("with--sidebar");
-    });
 
-    $("#site-cache").click(function(e) {
-      $("body").removeClass("with--sidebar");
-    });
-  })(jQuery);
-});
+    document.querySelectorAll('nav.menu a').forEach(addToggleMenuEvent);
+};
