90472783f276593912d63a593aa75a00791877d7
Joao Pedro Renan <jota.renan@gmail.com>
2017-09-19 18:03:24 -0300

    prettier test

--- app/components/client/acompanhar-pedido/acompanhar-pedido-controller.js
+++ app/components/client/acompanhar-pedido/acompanhar-pedido-controller.js
@@ -1,35 +1,39 @@
-angular.module('leMaitre')
-.controller('AcompanharPedidoCtrl', function($scope){
+angular.module('leMaitre').controller('AcompanharPedidoCtrl', function($scope) {
   $scope.items = [
     {
       name: 'Suco de Acerola',
       quantity: 3,
-      img: 'http://www.polpaideal.com.br/wp-content/uploads/2013/11/acerola.png',
+      img:
+        'http://www.polpaideal.com.br/wp-content/uploads/2013/11/acerola.png',
       status: 'D',
-      price: 5.90,
+      price: 5.9
     },
     {
       name: 'Suco de Maçã',
       quantity: 1,
-      img: 'http://armazemseuluiz.com.br/image/cache/catalog/BEBIDAS/Sucos%20PNG/Suco%20de%20Maca%20Integral%20Suco%20e%20So%201L-540x540.png',
+      img:
+        'http://armazemseuluiz.com.br/image/cache/catalog/BEBIDAS/Sucos%20PNG/Suco%20de%20Maca%20Integral%20Suco%20e%20So%201L-540x540.png',
       status: 'T',
-      price: 6.80,
+      price: 6.8
     },
     {
       name: 'Suco de Pêssego',
       quantity: 1,
-      img: 'http://armazemseuluiz.com.br/image/cache/catalog/BEBIDAS/Sucos%20PNG/Suco%20de%20Maca%20Integral%20Suco%20e%20So%201L-540x540.png',
+      img:
+        'http://armazemseuluiz.com.br/image/cache/catalog/BEBIDAS/Sucos%20PNG/Suco%20de%20Maca%20Integral%20Suco%20e%20So%201L-540x540.png',
       status: 'R',
-      price: 6.80,
+      price: 6.8
     }
   ];
 
-  $scope.pricesSum = $scope.items.reduce( (partialSum, item) => {
-    return partialSum + item.quantity * item.price;
-  }, 0).toFixed(2);
+  $scope.pricesSum = $scope.items
+    .reduce((partialSum, item) => {
+      return partialSum + item.quantity * item.price;
+    }, 0)
+    .toFixed(2);
 
   $scope.getCircleStatusClass = status => {
-    switch(status) {
+    switch (status) {
       case 'T': //todo
         return 'red';
       case 'D': //doing
