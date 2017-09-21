3ae54957abca1fae9a30125e60487b1e0aac2f46
Joao Pedro Renan <jota.renan@gmail.com>
2017-09-21 10:28:53 -0300

    lays base for adicionar-item controller

--- app/components/manager/adicionar-item/adicionar-item-controller.js
+++ app/components/manager/adicionar-item/adicionar-item-controller.js
@@ -1,4 +1,14 @@
 angular.module('leMaitre')
-.controller('AddItemCtrl', function($scope){
+.controller('AddItemCtrl',['$scope', '$http', function($scope, $http){
 
-});
+  $scope.item = {};
+
+  $scope.register = () => {
+    $http({
+      method: 'GET',
+      url: ``,
+      data: {name: name, picture: picture, price: price, description: description}  = $scope.item
+    });
+  };
+  
+}]);
