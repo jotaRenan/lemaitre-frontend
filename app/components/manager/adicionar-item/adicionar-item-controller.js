d42e82ae28d6f85b0d674c926a4a01333a495370
Joao Pedro Renan <jota.renan@gmail.com>
2017-09-24 17:05:08 -0300

    minor implementations on the adicionar item page

--- app/components/manager/adicionar-item/adicionar-item-controller.js
+++ app/components/manager/adicionar-item/adicionar-item-controller.js
@@ -1,14 +1,31 @@
 angular.module('leMaitre')
-.controller('AddItemCtrl',['$scope', '$http', function($scope, $http){
+.controller('AddItemCtrl',['$scope', '$http', '$state', function($scope, $http, $state){
 
-  $scope.item = {};
+  $scope.item = {
+    name: undefined,
+    picture: undefined,
+    price: undefined,
+    description: undefined,
+  };
 
   $scope.register = () => {
+    const {name: name, picture: picture, price: price, description: description}  = $scope.item;
     $http({
       method: 'GET',
+      // TODO: define servlet url
       url: ``,
-      data: {name: name, picture: picture, price: price, description: description}  = $scope.item
-    });
+      data: {name: name, picture: picture, price: price, description: description},
+    })
+      .then(response => {
+        alert(`Item ${name} adicionado!`);
+      })
+      .catch(response => {
+        alert(`Erro ${response.status}: ${response.statusText}`);
+      });
   };
-  
+
+  $scope.cancel = () => {
+    $state.go('home');
+  };
+
 }]);
