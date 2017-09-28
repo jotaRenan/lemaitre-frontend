ecc54a49a4a9952397305da2eada31a92d92b78d
jotarenan <jota.renan@gmail.com>
2017-09-28 11:55:21 -0300

    adds filter and css classes to cardapio page

--- app/components/client/cardapio/cardapio.controller.js
+++ app/components/client/cardapio/cardapio.controller.js
@@ -1,28 +1,45 @@
 angular.module('leMaitre')
 .controller('CardapioCtrl', function($scope){
 
-$scope.categorias = [
-  {
-    name: 'bebidas',
-    items: [{
-      name: 'Suco de Acerola',
-      price: 5.90,
+  $scope.categorias = [
+    {
+      name: 'bebidas',
+      items: [
+        {
+          name: 'Suco de Acerola',
+          price: 5.90,
+        },
+        {
+          name: 'Suco de Maçã',
+          price: 6.80,
+        },
+        {
+          name: 'Suco de Pêssego',
+          price: 6.80,
+        }
+      ],
     },
     {
-      name: 'Suco de Maçã',
-      price: 6.80,
+      name: 'drinks',
+      items: [
+        {
+          name: 'Suco de Acerola',
+          price: 5.90,
+        },
+        {
+          name: 'Suco de Maçã',
+          price: 6.80,
+        },
+        {
+          name: 'Suco de Pêssego',
+          price: 6.80,
+        }
+      ],
     },
-    {
-      name: 'Suco de Pêssego',
-      price: 6.80,
-    }],
-  }];
-  
-/*TODO
-$scope.showItems = () => {
-  let a = document.querySelector(".categoria-cardapio").childNodes;
-  console.log(a[4]);
-  a[4].classList.toggle("show-items");
-}
-*/
+  ];
+
+  $scope.showItems = (categoria) => {
+    document.querySelector(`.category-${categoria}`).classList.toggle('not-displayed');
+  }
+
 });
