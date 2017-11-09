b15c46e1503cccecc527dcbab8773deab8b7686c
jota renan <jota.renan@gmail.com>
2017-11-09 12:55:26 -0200

    dynamic display of tables

--- app/components/manager/gestao-mesas/gestao-mesas.controller.js
+++ app/components/manager/gestao-mesas/gestao-mesas.controller.js
@@ -1,3 +1,20 @@
 angular.module('leMaitre')
 .controller('GestaoMesasCtrl', function($scope){
+  $scope.tables = [
+    {
+      id: undefined,
+      status: undefined,
+      seats: undefined
+    },
+    {
+      id: undefined,
+      status: undefined,
+      seats: undefined
+    },
+  ];
+
+  $scope.getTableStatusClass = (status) => {
+    // TODO logic
+  }
+
 });
