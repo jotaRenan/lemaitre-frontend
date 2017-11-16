af487fddf46daf85f7cf9735eedfce6b840713d4
jota renan <jota.renan@gmail.com>
2017-11-16 11:40:47 -0200

    injects xeditable

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-angular.module("leMaitre", ["ui.router"]).config(function($locationProvider, $urlRouterProvider, $stateProvider){
+angular.module("leMaitre", ["ui.router", 'xeditable']).config(function($locationProvider, $urlRouterProvider, $stateProvider){
     $stateProvider
     	// Rota para a página Login
 	    .state('home', {
