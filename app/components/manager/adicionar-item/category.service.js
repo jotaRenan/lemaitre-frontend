54f1ead7863d655049e19b25da6e35d4378ba1dd
Joao Pedro Renan <jota.renan@gmail.com>
2017-11-25 17:02:30 -0200

    implements part of Categories service

---
+++ app/components/manager/adicionar-item/category.service.js
@@ -0,0 +1,35 @@
+angular.module('leMaitre')
+.factory('categoryManagementFactory', ['$http', 'apiEndpoint', function($http, apiEndpoint){
+  const categoryBaseURL = `${apiEndpoint}/webresources/category`;
+
+  return {
+    // RETRIEVES ALL CATEGORIES
+    retrieveCategories: function() {
+      return $http({
+        method: 'GET',
+        url: `${categoryBaseURL}`
+      });
+    },
+    // RETRIEVES CATEGORY BY ID
+    retrieveCategory: function(categoryID) {
+      return $http({
+        method: 'GET',
+        url: `${categoryBaseURL}/${categoryID}`,
+      });
+    },
+    // DELETES CATEGORY BY ID
+    deleteItem: function(categoryID) {
+      return $http({
+        method: 'DELETE',
+        url: `${categoryBaseURL}/${categoryID}`,
+      });
+    },
+    // RETRIEVES ALL ITEMS FROM A GIVEN CATEGORY
+    getItemsFromCategory: function(categoryID) {
+      return $http({
+        method: 'GET',
+        url: `${categoryBaseURL}/${categoryID}/items`,
+      });
+    }
+  };
+}]);
