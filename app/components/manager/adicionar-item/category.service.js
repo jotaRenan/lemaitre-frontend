becc83fe68d10a9ceed4033a1aa2ab6363fa6218
Joao Pedro Renan <jota.renan@gmail.com>
2017-11-25 17:52:44 -0200

    adds syntaxJSONSugar to categories service

--- app/components/manager/adicionar-item/category.service.js
+++ app/components/manager/adicionar-item/category.service.js
@@ -30,6 +30,12 @@ angular.module('leMaitre')
         method: 'GET',
         url: `${categoryBaseURL}/${categoryID}/items`,
       });
+    },
+    categoryJSONSyntaxSugar: function(badSyntax) {
+      let category = {};
+      category.name = badSyntax.nomCategory;
+      category.id = badSyntax.seqCategory;
+      category.subcategories = [];
     }
   };
 }]);
