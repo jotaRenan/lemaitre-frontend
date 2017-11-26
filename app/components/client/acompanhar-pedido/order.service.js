394f2f3f95518bebf456c24e0b28e7febc86e445
Joao Pedro Renan <jota.renan@gmail.com>
2017-11-25 23:58:59 -0200

    adds placeOrder service

--- app/components/client/acompanhar-pedido/order.service.js
+++ app/components/client/acompanhar-pedido/order.service.js
@@ -1,13 +1,22 @@
 angular.module('leMaitre')
-.factory('orderManagementFactory', ['$http', 'apiEndpoint', function($http, apiEndpoint){
+.factory('orderManagementFactory', ['$http', '$localStorage', 'apiEndpoint', function($http, $localStorage, apiEndpoint){
 
   const orderBaseURL = `${apiEndpoint}/webresources/order/`;
-  
+
   return {
     retrieveOrdersByToken: function(token) {
       return $http({
         method: 'GET',
         url: `${orderBaseURL}/${token}`,
+      });
+    },
+
+    placeOrder: function(order) {
+      return $http({
+        method: 'GET',
+        url: `${apiEndpoint}/OrderCreate`,
+        // data: {codToken: $localStorage.token, items: order.items}
+        data: {codToken: 'teste4321', items: order.items}
       });
     }
   };
