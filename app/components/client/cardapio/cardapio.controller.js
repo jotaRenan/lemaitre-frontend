e26d3310f881a61d285d442eaae739f336803698
Joao Pedro Renan <jota.renan@gmail.com>
2017-11-26 18:42:36 -0200

    retrieves everything

--- app/components/client/cardapio/cardapio.controller.js
+++ app/components/client/cardapio/cardapio.controller.js
@@ -1,5 +1,8 @@
 angular.module('leMaitre')
-.controller('CardapioCtrl', function($scope){
+.controller('CardapioCtrl', ['$scope', 'categoryManagementFactory', 'subcategoryManagementFactory', 'itemManagementFactory',
+function($scope, categoryManagementFactory, subcategoryManagementFactory, itemManagementFactory){
+
+  $scope.menu = [];
 
   $scope.categorias = [
     {
@@ -38,8 +41,47 @@ angular.module('leMaitre')
     },
   ];
 
-  $scope.showItems = (categoria) => {
-    document.querySelector(`.category-${categoria}`).classList.toggle('not-displayed');
-  }
+  const exhibitError = error => {
+    alert(`Erro ${error.status || error.name}: ${error.statusText || error.message}`);
+  };
 
-});
+  const retrieveCategories = () => {
+    categoryManagementFactory.retrieveCategories()
+      .then( response => {
+        const categories = response.data.content.map(categoryManagementFactory.categoryJSONSyntaxSugar);
+        categories.map(retrieveSubcategories);
+        $scope.menu.push(...categories);
+      })
+      .catch( error => exhibitError(error) );
+  };
+
+  const retrieveSubcategories = (category) => {
+    categoryManagementFactory.getSubcategoriesFromCategory(category.id)
+      .then( response => {
+        const subcategories = response.data.content.map(subcategoryManagementFactory.subcategoryJSONSyntaxSugar);
+        category.subcategories = [...subcategories];
+        subcategories.map(subcat => retrieveItemsFromSubcategory(subcat));
+      })
+      .catch( error => exhibitError(error) );
+  };
+
+  const retrieveItemsFromSubcategory = (subcategory) => {
+    categoryManagementFactory.getItemsFromSubcategory(subcategory.id)
+      .then( response => {
+        const items =  response.data.content.map(itemManagementFactory.itemJSONSyntaxSugar);
+        subcategory.items = [...items];
+      })
+      .catch(error => exhibitError(error));
+  };
+
+  $scope.tst = () => {
+    console.table($scope.menu);
+  };
+
+  $scope.showItems = (category) => {
+    document.querySelector(`.category-${category}`).classList.toggle('not-displayed');
+  };
+
+  retrieveCategories();
+
+}]);
