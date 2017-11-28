6c69aef8e4800989e59f19b6ac31113ae5e71e71
Joao Pedro Renan <jota.renan@gmail.com>
2017-11-27 23:51:30 -0200

    improves login system

--- app/components/client/acompanhar-pedido/bill.service.js
+++ app/components/client/acompanhar-pedido/bill.service.js
@@ -1,14 +1,21 @@
 angular.module('leMaitre')
-.factory('billManagementFactory', ['$http', 'apiEndpoint', '$localStorage', function($http, apiEndpoint, $localStorage){
+.factory('billManagementFactory', ['$http', 'apiEndpoint', '$sessionStorage', function($http, apiEndpoint, $sessionStorage){
 
   const billBaseURL = `${apiEndpoint}/webresources/bill`;
 
   return {
     //RETRIEVE BILL BY TOKEN
-    retrieveBill: function(token = $localStorage.token) {
+    retrieveBill: function(token = $sessionStorage.token) {
       return $http({
         method: 'GET',
         url: `${billBaseURL}/${token}`,
+      });
+    },
+    // RETRIEVE ALL BILLS AND TOKENS
+    retrieveAllBills: function() {
+      return $http({
+        method: 'GET',
+        url: `${billBaseURL}`,
       });
     },
     // EDIT BILL
--- app/components/shared/login/login.controller.js
+++ app/components/shared/login/login.controller.js
@@ -1,5 +1,5 @@
 angular.module('leMaitre')
-.controller('LoginCtrl', ['$timeout','$scope', '$sessionStorage', 'loginManagementFactory', 'tokenManagementFactory', function($timeout, $scope, $sessionStorage, loginManagementFactory, tokenManagementFactory){
+.controller('LoginCtrl', ['$timeout','$scope', '$sessionStorage', 'loginManagementFactory', 'tokenManagementFactory', 'billManagementFactory', '$state', function($timeout, $scope, $sessionStorage, loginManagementFactory, tokenManagementFactory, billManagementFactory, $state){
   const rightUser = 'manager';
   const rightPass = '123';
 
@@ -44,6 +44,23 @@ angular.module('leMaitre')
       username: '',
       password: ''
     };
+  };
+
+  $scope.checkToken = (token) => {
+    billManagementFactory.retrieveAllBills()
+      .then( response => {
+        const tokenMatches = response.data.content
+          .map(billManagementFactory.billJSONSugar)
+          .find(bill => bill.token === token);
+        if (tokenMatches) {
+          $scope.$storage.isLogged = true;
+          $scope.$storage.token = token;
+          $state.go('cardapio');
+        } else {
+          throw new Error('token doesnt match');
+        }
+      })
+      .catch(err => exhibitError(err));
   };
 
   // Begin execution
